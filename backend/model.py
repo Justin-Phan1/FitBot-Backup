4f64ccee8b8b6b37dd100266b365d68af1b8d864
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2025-01-09 14:32:39 -0500

    Create the model

---
+++ backend/model.py
@@ -0,0 +1,24 @@
+import torch 
+import torch.nn as nn
+import os 
+import sys
+
+# Use for issues regarding importing modules
+file_dir = os.path.dirname(__file__)
+sys.path.append(file_dir)
+
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNetwork, self).__init__()
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.l2 = nn.Linear(hidden_size, hidden_size)
+        self.l3 = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        output = self.l1(x)
+        output = self.relu(output)
+        output = self.l2(output)
+        output = self.relu(output)
+        output = self.l3(output)
+        return output
