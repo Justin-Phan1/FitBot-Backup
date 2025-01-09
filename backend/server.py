e0ce0679c0471af665599dcd69740aa5d67d0e5f
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2025-01-09 14:38:05 -0500

    Create Flask server

---
+++ backend/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from backend.model import NeuralNetwork # Import the model from model.py
+from backend.chatbot import get_response # Import the chat function from chatbot.py
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    text = data.get('message')
+    response = get_response(text)
+    return jsonify({"answer": response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
