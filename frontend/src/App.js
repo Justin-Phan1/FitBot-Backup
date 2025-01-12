6640e50dc976a96ff891b72fc2ab6b93bc64666d
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2025-01-11 23:47:23 -0500

    Create Chatbot app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function Fitbot() {
+  const [input, setInput] = useState('');           
+  const [response, setResponse] = useState('');    
+
+    // 
+    const fetchData = async () => {
+      try {
+        const result = await axios.post("http://127.0.0.1:5000/chat", { message: input });
+        
+        setResponse(result.data.answer); 
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p>Welcome to FitBot</p>
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}  
+          placeholder="Type your message"
+        />
+      </div>
+      <button onClick={fetchData}>Send</button> 
+      
+      {response}
     </div>
   );
 }
 
-export default App;
+export default Fitbot;
