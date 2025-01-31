7fe810fa2312368eaf97bde20d13e2fa343ebe8c
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2025-01-31 12:44:13 -0500

    Change import path

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from backend.model import NeuralNetwork # Import the model from model.py
-from backend.chatbot import get_response # Import the chat function from chatbot.py
+from model import NeuralNetwork # Import the model from model.py
+from chatbot import get_response # Import the chat function from chatbot.py
 
 app = Flask(__name__)
 CORS(app)
@@ -14,4 +14,4 @@ def chat():
     return jsonify({"answer": response})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
